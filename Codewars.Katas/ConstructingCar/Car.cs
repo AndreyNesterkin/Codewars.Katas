1f96582a6b36338097b4a9fef9ed877bcb8508e4
Andrey Nesterkin <nesterkin.am@outlook.com>
2017-02-24 17:26:26 +0300

    Publish

--- Codewars.Katas/ConstructingCar/Car.cs
+++ Codewars.Katas/ConstructingCar/Car.cs
@@ -6,25 +6,10 @@ namespace Codewars.Katas.ConstructingCar
     public class Car : ICar
     {
         private const int DefaultAcceleration = 10;
-        private const int MaxAcceptableAcceleration = 20;
-        private const int MinAcceptableAcceleration = 5;
-        private const int DefaultBreak = 10;
         private const int FreeWheelSlowingDown = 1;
-        private const int MaxSpeedLimit = 250;
-
-        private Tuple<int, int, double>[] _consumptionOnAccelerateTable = new Tuple<int, int, double>[]
-        {
-            new Tuple<int, int, double>(1, 60, 0.0020d),
-            new Tuple<int, int, double>(61, 100, 0.0014d),
-            new Tuple<int, int, double>(101, 140, 0.0020d),
-            new Tuple<int, int, double>(141, 200, 0.0025d),
-            new Tuple<int, int, double>(201, MaxSpeedLimit, 0.0030d)
-        };
 
         private const double DefaultFuelLevel = 20.0d;
         private const double ConsumptionOnIdle = 0.0003d;
-
-        private int _maxAcceleration;
 
         private IEngine _engine;
 
@@ -53,22 +38,9 @@ namespace Codewars.Katas.ConstructingCar
 
             fuelTankDisplay = new FuelTankDisplay(_fuelTank);
 
-            _maxAcceleration = GetMaxAcceptableAcceleration(maxAcceleration);
-
-            _drivingProcessor = new DrivingProcessor();
+            _drivingProcessor = new DrivingProcessor(_engine, maxAcceleration);
 
             drivingInformationDisplay = new DrivingInformationDisplay(_drivingProcessor);
-        }
-
-        private int GetMaxAcceptableAcceleration(int maxAcceleration)
-        {
-            if (maxAcceleration > MaxAcceptableAcceleration)
-                return MaxAcceptableAcceleration;
-
-            if (maxAcceleration < MinAcceptableAcceleration)
-                return MinAcceptableAcceleration;
-
-            return maxAcceleration;
         }
 
         public bool EngineIsRunning
@@ -101,73 +73,15 @@ namespace Codewars.Katas.ConstructingCar
 
         public void BrakeBy(int speed)
         {
-            if (_drivingProcessor.ActualSpeed == 0)
-                return;
-
-            var brake = GetActualBreak(speed);
-
-            _drivingProcessor.ReduceSpeed(brake);
-        }
-
-        private int GetActualBreak(int speed)
-        {
-            return speed > DefaultBreak ? DefaultBreak : speed;
+            _drivingProcessor.ReduceSpeed(speed);
         }
 
         public void Accelerate(int speed)
         {
-            if (speed <= 0 || !_engine.IsRunning)
-                return;
-
-            speed = LimitSpeed(speed);
-
             if (speed >= _drivingProcessor.ActualSpeed)
-                AccelerateTo(speed);
+                _drivingProcessor.IncreaseSpeedTo(speed);
             else
                 FreeWheel();
-        }
-
-        private void AccelerateTo(int speed)
-        {
-            IncreaseSpeedTo(speed);
-
-            ConsumeFuelOnAccelerate();
-        }
-
-        private int LimitSpeed(int speed)
-        {
-            if (speed > MaxSpeedLimit)
-                return MaxSpeedLimit;
-
-            return speed;
-        }
-
-        private void IncreaseSpeedTo(int speed)
-        {
-            var acceleration = GetActualAcceleration(speed);
-
-            _drivingProcessor.IncreaseSpeedTo(acceleration);
-        }
-
-        private int GetActualAcceleration(int speed)
-        {
-            var requiredAcceleration = speed - _drivingProcessor.ActualSpeed;
-
-            return requiredAcceleration > _maxAcceleration ? _maxAcceleration : requiredAcceleration;
-        }
-
-        private void ConsumeFuelOnAccelerate()
-        {
-            var consumption = GetConsumptionOnAccelerate();
-
-            _engine.Consume(consumption);
-        }
-
-        private double GetConsumptionOnAccelerate()
-        {
-            var speed = _drivingProcessor.ActualSpeed;
-
-            return _consumptionOnAccelerateTable.First(t => speed >= t.Item1 && speed <= t.Item2).Item3;
         }
 
         public void FreeWheel()
--- Codewars.Katas/ConstructingCar/DrivingProcessor.cs
+++ Codewars.Katas/ConstructingCar/DrivingProcessor.cs
@@ -1,7 +1,44 @@
-﻿namespace Codewars.Katas.ConstructingCar
+﻿using System;
+using System.Linq;
+
+namespace Codewars.Katas.ConstructingCar
 {
     public class DrivingProcessor : IDrivingProcessor
     {
+        private const int MaxSpeedLimit = 250;
+        private const int MaxAcceptableAcceleration = 20;
+        private const int MinAcceptableAcceleration = 5;
+        private const int DefaultBreak = 10;
+        private Tuple<int, int, double>[] _consumptionOnAccelerateTable = new Tuple<int, int, double>[]
+           {
+                new Tuple<int, int, double>(1, 60, 0.0020d),
+                new Tuple<int, int, double>(61, 100, 0.0014d),
+                new Tuple<int, int, double>(101, 140, 0.0020d),
+                new Tuple<int, int, double>(141, 200, 0.0025d),
+                new Tuple<int, int, double>(201, MaxSpeedLimit, 0.0030d)
+           };
+
+        private IEngine _engine;
+        private int _maxAcceleration;
+
+        public DrivingProcessor(IEngine engine, int maxAcceleration)
+        {
+            _engine = engine;
+
+            _maxAcceleration = GetMaxAcceptableAcceleration(maxAcceleration);
+        }
+
+        private int GetMaxAcceptableAcceleration(int maxAcceleration)
+        {
+            if (maxAcceleration > MaxAcceptableAcceleration)
+                return MaxAcceptableAcceleration;
+
+            if (maxAcceleration < MinAcceptableAcceleration)
+                return MinAcceptableAcceleration;
+
+            return maxAcceleration;
+        }
+
         public int ActualSpeed
         {
             get; private set;
@@ -9,12 +46,59 @@
 
         public void IncreaseSpeedTo(int speed)
         {
-            ActualSpeed += speed;
+            if (speed <= 0 || !_engine.IsRunning)
+                return;
+
+            Accelerate(speed);
+
+            ConsumeFuelOnAccelerate(ActualSpeed);
+        }
+
+        private void Accelerate(int speed)
+        {
+            var acceleration = GetActualAcceleration(speed);
+
+            ActualSpeed = LimitSpeed(ActualSpeed + acceleration);
+        }
+
+        private int GetActualAcceleration(int speed)
+        {
+            var requiredAcceleration = speed - ActualSpeed;
+
+            return requiredAcceleration > _maxAcceleration ? _maxAcceleration : requiredAcceleration;
+        }
+
+        private void ConsumeFuelOnAccelerate(int actualSpeed)
+        {
+            var consumption = GetFuelConsumption(actualSpeed);
+
+            _engine.Consume(consumption);
+        }
+
+        private double GetFuelConsumption(int speed)
+        {
+            return _consumptionOnAccelerateTable.First(t => speed >= t.Item1 && speed <= t.Item2).Item3;
         }
 
         public void ReduceSpeed(int speed)
         {
-            ActualSpeed -= speed;
+            if (ActualSpeed == 0)
+                return;
+
+            ActualSpeed -= GetActualBreak(speed);
+        }
+
+        private int GetActualBreak(int speed)
+        {
+            return speed > DefaultBreak ? DefaultBreak : speed;
+        }
+
+        private int LimitSpeed(int speed)
+        {
+            if (speed > MaxSpeedLimit)
+                return MaxSpeedLimit;
+
+            return speed;
         }
     }
 }
